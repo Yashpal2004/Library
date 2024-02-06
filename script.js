a1f5ebbbd5b5fb610d1c0fb76b3c85948dc2f24b
Yashpal2004 <meyashpal2004@gmail.com>
2024-02-06 23:21:11 +0530

    basic

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
+
 let myLibrary = [];
 
-function Book() {
+window.onload = function() {
+
+
+  const dialog = document.querySelector("dialog");
+  const showButton = document.querySelector("dialog + button");
+  const closeButton = document.querySelector("dialog button");
+  const cards = document.getElementById("cards");
+  
+
+  closeButton.addEventListener("click", () => {
+    let authorInput = document.getElementById("bookauthor").value;
+    let titleInput = document.getElementById("booktitle").value;
+    addBookToLibrary(titleInput, authorInput);
+  })
+
+  function Book() {
+    const card = cards.appendChild(document.createElement("div"));
+    card.classList.add("card");
+    const cardTitle = card.appendChild(document.createElement("p"));
+    const cardAuthor = card.appendChild(document.createElement("p"));      
+    cardTitle.innerHTML = myLibrary[myLibrary.length - 1].title;
+    cardAuthor.innerHTML = myLibrary[myLibrary.length - 1].author;
+
+  }
+
+  function addBookToLibrary(a, b) {
+    const x = new Object();
+    x.title = a;
+    x.author = b;
+
+    myLibrary.push(x);
+    console.log(myLibrary)
+
+    Book();
+  }
+
+
+  showButton.addEventListener("click", () => {
+    dialog.showModal();
+  });
+
+  closeButton.addEventListener("click", () => {
+    dialog.close();
+  });
 
 }
-
-function addBookToLibrary() {
-
-}
-
-
-
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
