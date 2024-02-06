c5455495499bc0b0cf933bf5aacc87ac42de32f6
Yashpal2004 <meyashpal2004@gmail.com>
2024-02-06 21:28:35 +0530

    form

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
